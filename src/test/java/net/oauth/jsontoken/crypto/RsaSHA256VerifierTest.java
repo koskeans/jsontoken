f52f357347e53f02db3a171f88af68ddbd8b9845
Anssi Koskela <anssi.koskela@greenergrass.company>
2016-02-22 18:40:12 +0200

    Update the test to verify single signature

--- src/test/java/net/oauth/jsontoken/crypto/RsaSHA256VerifierTest.java
+++ src/test/java/net/oauth/jsontoken/crypto/RsaSHA256VerifierTest.java
@@ -1,11 +1,13 @@
 package net.oauth.jsontoken.crypto;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
@@ -13,36 +15,46 @@ import junit.framework.TestCase;
 
 public class RsaSHA256VerifierTest extends TestCase {
 
+    private byte[] testDatas;
+    private byte[] signatures;
+    private RsaSHA256Verifier verifier;
+
     public void testVerifySignatureInThreads() throws Exception {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        KeyPair keyPair = keyGen.generateKeyPair();
-        Signature sig = Signature.getInstance("SHA256withRSA");
-        PublicKey publicKey = keyPair.getPublic();
-        sig.initSign(keyPair.getPrivate());
-        final byte[] testData = "something that is signed.".getBytes("utf-8");
-        sig.update(testData);
-        final byte[] signature = sig.sign();
-        final RsaSHA256Verifier verifier = new RsaSHA256Verifier(publicKey);
         final Set<Integer> complete = new ConcurrentSkipListSet<Integer>();
         int threads = 5000;
         for(int i = 0; i < threads ; ++i){
             final int j = i;
-            Runnable r = new Runnable() {
+            new Thread(new Runnable() {
                 public void run() {
                     try {
-                        verifier.verifySignature(testData, signature);
+                        verifier.verifySignature(testDatas, signatures);
                         complete.add(j);
                     } catch (SignatureException e) {
                         complete.add(j);
                         throw new AssertionError("Could not verify signature", e);                        
                     }
                 }
-            };
-            new Thread(r).start();
+            }).start();
         }
         while(complete.size() < threads){
             // wait...
         }
+    }
+    
+    public void testVerifySignature() throws Exception {
+        verifier.verifySignature(testDatas, signatures);
+    }
+
+    protected void setUp()
+            throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        KeyPair keyPair = keyGen.generateKeyPair();
+        Signature sig = Signature.getInstance("SHA256withRSA");
+        sig.initSign(keyPair.getPrivate());
+        testDatas = "something that is signed.".getBytes("utf-8");
+        sig.update(testDatas);
+        signatures = sig.sign();
+        verifier = new RsaSHA256Verifier(keyPair.getPublic());
     }
     
 }
